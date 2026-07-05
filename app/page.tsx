297326e3f85e9decc3939fc03e477bbf8db94a76
gokm8 <gozbayir@hotmail.com>
2026-07-05 23:51:16 +0200

    Add CalendarComponent with mock data

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
+import CalendarComponent from "@/components/Calendar";
+
 export default function Home() {
+  const events = [
+    {
+      start: new Date(2026, 6, 6, 10, 0),
+      end: new Date(2026, 6, 6, 20, 0),
+      title: "Kok",
+    },
+    {
+      start: new Date(2026, 6, 7, 10, 0),
+      end: new Date(2026, 6, 7, 20, 0),
+      title: "Tjener",
+    },
+    {
+      start: new Date(2026, 6, 8, 10, 0),
+      end: new Date(2026, 6, 8, 20, 0),
+      title: "Opvasker",
+    },
+  ];
+
   return (
     <div>
       <h1>Vagtplanlægningssystem</h1>
+      <CalendarComponent events={events} />
     </div>
   );
 }
---
+++ components/Calendar.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import { format } from "date-fns/format";
+import { parse } from "date-fns/parse";
+import { startOfWeek } from "date-fns/startOfWeek";
+import { getDay } from "date-fns/getDay";
+import { da } from "date-fns/locale/da";
+
+// Messages in the calender UI
+const messages = {
+  today: "I dag",
+  previous: "Forrige",
+  next: "Næste",
+  month: "Måned",
+  week: "Uge",
+  day: "Dag",
+  agenda: "Agenda",
+  date: "Dato",
+  time: "Tid",
+  event: "Vagt",
+  noEventsInRange: "Ingen vagter i denne periode",
+  showMore: (total: number) => `+${total} mere`,
+};
+
+// Locale for the calendar
+const locales = {
+  da: da,
+};
+
+// Localizer for the calendar
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+export type Event = {
+  start: Date;
+  end: Date;
+  title: string;
+};
+
+export default function CalendarComponent({ events }: { events: Event[] }) {
+  return (
+    <div>
+      <Calendar
+        localizer={localizer}
+        messages={messages}
+        culture="da"
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
+    </div>
+  );
+}
