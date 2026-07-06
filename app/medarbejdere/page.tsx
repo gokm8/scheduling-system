7b8dcc498e9d3ec2922b47a9e4303077575d2fe8
gokm8 <gozbayir@hotmail.com>
2026-07-06 14:11:24 +0200

    Add EmployeeList component and integrate it into MedarbejderePage to display employee data

--- app/medarbejdere/page.tsx
+++ app/medarbejdere/page.tsx
@@ -1,9 +1,14 @@
 import EmployeeFormComponent from "@/components/EmloyeeForm";
+import EmployeeList from "@/components/EmployeeList";
+import { prisma } from "@/lib/prisma";
 
-export default function MedarbejderePage() {
+export default async function MedarbejderePage() {
+  const employees = await prisma.employee.findMany();
+
   return (
     <div>
       <EmployeeFormComponent />
+      <EmployeeList employees={employees} />
     </div>
   );
 }
---
+++ components/EmployeeList.tsx
@@ -0,0 +1,13 @@
+import { Employee } from "@prisma/client";
+
+export default function EmployeeList({ employees }: { employees: Employee[] }) {
+  return (
+    <div>
+      {employees.map((employee) => (
+        <div key={employee.id}>
+          {employee.name} - {employee.role} - {employee.email}
+        </div>
+      ))}
+    </div>
+  );
+}
